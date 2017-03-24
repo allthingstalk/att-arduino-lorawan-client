75e0154ca8553eec213d5ff5f8b55b1ba05f6e2d
Jan Bogaerts <jb@allthingstalk.com>
2017-03-24 16:24:05 +0100

    missing container.h

--- Container.h
+++ Container.h
@@ -23,6 +23,30 @@ Original author: Jan Bogaerts (2015-2017)
 #include <Stream.h>
 #include <LoRaPacket.h>
 
+
+//sensors
+#define BINARY_SENSOR ((short)1)
+#define BINARY_TILT_SENSOR ((short)2)
+#define PUSH_BUTTON ((short)3)
+#define DOOR_SENSOR ((short)4)
+#define TEMPERATURE_SENSOR ((short)5)
+#define LIGHT_SENSOR ((short)6)
+#define PIR_SENSOR ((short)7)
+#define ACCELEROMETER ((short)8)
+#define GPS ((short)9)
+#define PRESSURE_SENSOR ((short)10)
+#define HUMIDITY_SENSOR ((short)11)
+#define LOUDNESS_SENSOR ((short)12)
+#define AIR_QUALITY_SENSOR ((short)13)
+#define BATTERY_LEVEL ((short)14)
+#define INTEGER_SENSOR ((short)15)
+#define NUMBER_SENSOR ((short)16)
+
+//actuators
+#define BUZZER ((short)1)
+#define RELAY ((short)2)
+#define LED ((short)3)
+
 /** 
  A helper class for sending data in the proximus-container data format.
  
@@ -50,6 +74,8 @@ class Container: public LoRaPacket
 		- id: the container id, see [here](http://docs.allthingstalk.com/developers/libraries/lora/#Supported-sensors-and-actuators) 
 		  for more info on the available containers.
 		- ack: when true, acknowledgement is requested from the base station, otherwise no acknowledge is waited for.
+    
+    returns: true upon success.
 		*/
 		bool Send(bool value, short id, bool ack = true);
 		
@@ -60,6 +86,8 @@ class Container: public LoRaPacket
 		- id: the container id, see [here](http://docs.allthingstalk.com/developers/libraries/lora/#Supported-sensors-and-actuators) 
 		  for more info on the available containers.
 		- ack: when true, acknowledgement is requested from the base station, otherwise no acknowledge is waited for.
+    
+    returns: true upon success.
 		*/
 		bool Send(short value, short id, bool ack = true);
 		
@@ -70,6 +98,8 @@ class Container: public LoRaPacket
 		- id: the container id, see [here](http://docs.allthingstalk.com/developers/libraries/lora/#Supported-sensors-and-actuators) 
 		  for more info on the available containers.
 		- ack: when true, acknowledgement is requested from the base station, otherwise no acknowledge is waited for.
+    
+    returns: true upon success.
 		*/
 		bool Send(String value, short id, bool ack = true);
 		
@@ -79,6 +109,8 @@ class Container: public LoRaPacket
 		- id: the container id, see [here](http://docs.allthingstalk.com/developers/libraries/lora/#Supported-sensors-and-actuators) 
 		  for more info on the available containers.
 		- ack: when true, acknowledgement is requested from the base station, otherwise no acknowledge is waited for.
+    
+    returns: true upon success.
 		*/
 		bool Send(float value, short id, bool ack = true);
 		
@@ -91,6 +123,8 @@ class Container: public LoRaPacket
 		- id: the container id, see [here](http://docs.allthingstalk.com/developers/libraries/lora/#Supported-sensors-and-actuators) 
 		  for more info on the available containers.
 		- ack: when true, acknowledgement is requested from the base station, otherwise no acknowledge is waited for.
+    
+    returns: true upon success.
 		*/
 		bool Send(float x, float y, float z, short id, bool ack = true);
 	
@@ -103,9 +137,22 @@ class Container: public LoRaPacket
 		- id: the container id, see [here](http://docs.allthingstalk.com/developers/libraries/lora/#Supported-sensors-and-actuators) 
 		  for more info on the available containers.
 		- ack: when true, acknowledgement is requested from the base station, otherwise no acknowledge is waited for.
+    
+    returns: true upon success.
 		*/
 		bool Send(float x, float y, float z, float time, short id, bool ack = true);
 		
+    /** extract the data and container type from a downlink message (message from cloud to device).
+    
+    parameters:
+    - data: a pointer to the data structure that contains the raw payload.
+    - id: a reference to a variable that will receive the id of the container type contained in the message.
+    
+    returns: the address to the first byte of the data stream. It is up to the client to interprete the pointer according to the data type
+             normally expected for the specified container id.
+    **/
+    const uint8_t* Parse(const uint8_t* data, short& id);
+    
 	protected:
 	
 		///writes the packet content to the specified byte array. This must be at least 51 bytes long.
